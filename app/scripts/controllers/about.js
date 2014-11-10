2fae9ae772ad68ac35a46c26fc084d04cab79274
dakaney <wayman_ng@yahoo.com>
2014-11-09 17:35:28 -0800

    assignment 3

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dakaneyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dakaneyApp
+ */
+angular.module('dakaneyApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
